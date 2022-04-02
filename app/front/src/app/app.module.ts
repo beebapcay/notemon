c6e2b13ff24a077736ddd8d03b5ca684534fab0c
Phat Duong <beebapcay@gmail.com>
2022-04-02 10:08:43 +0700

    Add enum folder, add top-navbar component, add angular material and bootstrap

--- app/front/src/app/app.module.ts
+++ app/front/src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { TopNavbarComponent } from './component/top-navbar/top-navbar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, TopNavbarComponent],
+  imports: [BrowserModule, NgbModule, BrowserAnimationsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
