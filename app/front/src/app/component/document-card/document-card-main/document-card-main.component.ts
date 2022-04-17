043a0b9eae6da4022c9cef8fa1105f04713d4223
Phat Duong <beebapcay@gmail.com>
2022-04-17 18:33:39 +0700

    Add ui for `directory-card-main` and `document-card-main`

--- app/front/src/app/component/document-card/document-card-main/document-card-main.component.ts
+++ app/front/src/app/component/document-card/document-card-main/document-card-main.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {SizeEnum} from '../../../enum/size.enum';
 
 @Component({
   selector: 'app-document-card-main',
@@ -6,6 +7,8 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./document-card-main.component.scss']
 })
 export class DocumentCardMainComponent implements OnInit {
+
+  readonly SizeEnum = SizeEnum;
 
   constructor() {
   }
