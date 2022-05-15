0974a6539b2bdbb8e61b18352ad142648343dbd7
Phat Duong <beebapcay@gmail.com>
2022-05-16 01:55:23 +0700

    Add card action menu emitted

--- app/front/src/app/component/notemon-card/card-action-bar/card-action-bar.component.ts
+++ app/front/src/app/component/notemon-card/card-action-bar/card-action-bar.component.ts
@@ -1,5 +1,7 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {NotemonTypeEnum} from '../../../enum/notemon-type.enum';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { CardActionMenuEnum } from '../../../enum/card-action-nemu.enum';
+import { NotemonTypeEnum } from '../../../enum/notemon-type.enum';
+import { DocumentModel } from '../../../model/document.model';
 
 @Component({
   selector: 'card-action-bar',
@@ -7,7 +9,12 @@ import {NotemonTypeEnum} from '../../../enum/notemon-type.enum';
   styleUrls: ['./card-action-bar.component.scss']
 })
 export class CardActionBarComponent implements OnInit {
+  @Input() item: DocumentModel = null;
   @Input() type: NotemonTypeEnum = NotemonTypeEnum.DEFAULT;
+
+  @Output() menuActionEmitted = new EventEmitter<CardActionMenuEnum>(null);
+
+  readonly CardActionMenuEnum = CardActionMenuEnum;
 
   constructor() {
   }
