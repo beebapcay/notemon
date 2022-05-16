166a4a79a233b6912fcddcb3278e8c5891ab27a7
Phat Duong <beebapcay@gmail.com>
2022-05-16 21:15:45 +0700

    Config github action for azure

--- app/front/src/environments/environment.ts
+++ app/front/src/environments/environment.ts
@@ -1,3 +1,4 @@
+
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
