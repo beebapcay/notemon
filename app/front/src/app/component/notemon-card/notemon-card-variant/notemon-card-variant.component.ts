41260413bd185c31cd0c066dd934bcd1aa52ccb0
Phat Duong <beebapcay@gmail.com>
2022-05-16 10:50:55 +0700

    Remove unneccessary method in `notemon-card-variant`

--- app/front/src/app/component/notemon-card/notemon-card-variant/notemon-card-variant.component.ts
+++ app/front/src/app/component/notemon-card/notemon-card-variant/notemon-card-variant.component.ts
@@ -1,26 +1,17 @@
-import {
-  AfterViewChecked,
-  AfterViewInit,
-  Component,
-  ElementRef,
-  OnChanges,
-  OnInit,
-  SimpleChanges,
-  ViewChild
-} from '@angular/core';
-import { NotemonTypeEnum } from '../../../enum/notemon-type.enum';
-import { DocumentModel } from '../../../model/document.model';
-import { DocumentService } from '../../../service/document.service';
-import { SnackbarService } from '../../../service/snackbar.service';
-import { UserService } from '../../../service/user.service';
-import { DocumentCardAbstractComponent } from '../document-card.abstract.component';
+import {Component, ElementRef, ViewChild} from '@angular/core';
+import {NotemonTypeEnum} from '../../../enum/notemon-type.enum';
+import {DocumentModel} from '../../../model/document.model';
+import {DocumentService} from '../../../service/document.service';
+import {SnackbarService} from '../../../service/snackbar.service';
+import {UserService} from '../../../service/user.service';
+import {DocumentCardAbstractComponent} from '../document-card.abstract.component';
 
 @Component({
   selector: 'app-notemon-card-variant',
   templateUrl: './notemon-card-variant.component.html',
   styleUrls: ['./notemon-card-variant.component.scss']
 })
-export class NotemonCardVariantComponent extends DocumentCardAbstractComponent<DocumentModel> implements OnInit, OnChanges, AfterViewInit, AfterViewChecked {
+export class NotemonCardVariantComponent extends DocumentCardAbstractComponent<DocumentModel> {
   @ViewChild('nameInput') override nameInput: ElementRef;
 
   constructor(
@@ -28,26 +19,6 @@ export class NotemonCardVariantComponent extends DocumentCardAbstractComponent<D
     snackbarService: SnackbarService,
     documentService: DocumentService) {
     super(userService, snackbarService, documentService);
-  }
-
-  override ngOnInit() {
-    super.ngOnInit();
-  }
-
-  override ngOnChanges(changes: SimpleChanges) {
-    super.ngOnChanges(changes);
-  }
-
-  override ngAfterViewInit() {
-    super.ngAfterViewInit();
-  }
-
-  override onMenuItemClicked(actionOption: any) {
-    super.onMenuItemClicked(actionOption);
-  }
-
-  override ngAfterViewChecked() {
-    super.ngAfterViewChecked();
   }
 
   public getType(): NotemonTypeEnum {
