bf0390839f1548be39192a1f37be963cac79e6e1
Phat Duong <beebapcay@gmail.com>
2022-05-20 14:43:25 +0700

    Fix error redundant pipe

--- app/front/src/app/pipe/pipe.module.ts
+++ app/front/src/app/pipe/pipe.module.ts
@@ -5,11 +5,10 @@ import {HashtagPipe} from './hashtag.pipe';
 import {NotemonCardImgPipe} from './notemon-card-img.pipe';
 import {DurationFromNowPipe} from './duration-from-now.pipe';
 import {Seconds2timePipe} from './seconds2time.pipe';
-import {ContentSummaryPipe} from './content-summary.pipe';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [AuthBgImgPipe, HashtagPipe, NotemonCardImgPipe, DurationFromNowPipe, Seconds2timePipe, ContentSummaryPipe],
+  declarations: [AuthBgImgPipe, HashtagPipe, NotemonCardImgPipe, DurationFromNowPipe, Seconds2timePipe],
   exports: [CommonModule, AuthBgImgPipe, HashtagPipe, NotemonCardImgPipe, DurationFromNowPipe, Seconds2timePipe],
 })
 export class PipeModule {
