74c0dcd605348c0752b8b0e51ffaa4c3228fbde3
Phat Duong <beebapcay@gmail.com>
2022-05-23 00:13:14 +0700

    Add basic layout for settings draw sidebar

--- app/front/src/app/component/component.module.ts
+++ app/front/src/app/component/component.module.ts
@@ -1,44 +1,47 @@
-import {CommonModule} from '@angular/common';
-import {NgModule} from '@angular/core';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {BrowserModule} from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {RouterModule} from '@angular/router';
-import {CKEditorModule} from '@ckeditor/ckeditor5-angular';
-import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
-import {MaterialModule} from '../material.module';
-import {PipeModule} from '../pipe/pipe.module';
-import {ServiceModule} from '../service/service.module';
-import {AuthInputComponent} from './auth-page/auth-input/auth-input.component';
-import {AuthPageComponent} from './auth-page/auth-page.component';
-import {LoginFormComponent} from './auth-page/login-form/login-form.component';
-import {SignupFormComponent} from './auth-page/signup-form/signup-form.component';
-import {BottomNavbarComponent} from './bottom-navbar/bottom-navbar.component';
-import {DashboardPageComponent} from './dashboard-page/dashboard-page.component';
-import {ErrorPageComponent} from './error-page/error-page.component';
-import {FeaturePageComponent} from './feature-page/feature-page.component';
-import {FormFieldComponent} from './form-field/form-field.component';
-import {HashtagComponent} from './hashtag/hashtag.component';
-import {HomePageComponent} from './home-page/home-page.component';
-import {NewNotemonButtonComponent} from './new-notemon-button/new-notemon-button.component';
-import {NotePageComponent} from './note-page/note-page.component';
-import {CardActionBarComponent} from './notemon-card/card-action-bar/card-action-bar.component';
-import {DirectoryCardComponent} from './notemon-card/directory-card/directory-card.component';
-import {NoteCardComponent} from './notemon-card/note-card/note-card.component';
-import {NotemonCardEmptyComponent} from './notemon-card/notemon-card-empty/notemon-card-empty.component';
-import {NotemonCardVariantComponent} from './notemon-card/notemon-card-variant/notemon-card-variant.component';
-import {NotemonEditorComponent} from './notemon-editor/notemon-editor.component';
-import {SnackbarComponent} from './snackbar/snackbar.component';
-import {ThemeToggleButtonComponent} from './theme-toggle-button/theme-toggle-button.component';
-import {TopNavbarEditorComponent} from './top-navbar/top-navbar-editor/top-navbar-editor.component';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { MaterialModule } from '../material.module';
+import { PipeModule } from '../pipe/pipe.module';
+import { ServiceModule } from '../service/service.module';
+import { AuthInputComponent } from './auth-page/auth-input/auth-input.component';
+import { AuthPageComponent } from './auth-page/auth-page.component';
+import { LoginFormComponent } from './auth-page/login-form/login-form.component';
+import { SignupFormComponent } from './auth-page/signup-form/signup-form.component';
+import { BottomNavbarComponent } from './bottom-navbar/bottom-navbar.component';
+import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
+import { FeaturePageComponent } from './feature-page/feature-page.component';
+import { FormFieldComponent } from './form-field/form-field.component';
+import { HashtagComponent } from './hashtag/hashtag.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { LoadingProgressBarComponent } from './loading/loading-progress-bar/loading-progress-bar.component';
+import { LoadingProgressSpinnerComponent } from './loading/loading-progress-spinner/loading-progress-spinner.component';
+import { ManageDashboardBaseComponent } from './manage-dashboard-base/manage-dashboard-base.component';
+import { NewNotemonButtonComponent } from './new-notemon-button/new-notemon-button.component';
+import { NotePageComponent } from './note-page/note-page.component';
+import {
+  OptionSidebarItemComponent
+} from './note-page/option-sidebar/option-sidebar-item/option-sidebar-item.component';
+import { OptionSidebarComponent } from './note-page/option-sidebar/option-sidebar.component';
+import { SettingsDrawSidebarComponent } from './note-page/settings-draw-sidebar/settings-draw-sidebar.component';
+import { CardActionBarComponent } from './notemon-card/card-action-bar/card-action-bar.component';
+import { DirectoryCardComponent } from './notemon-card/directory-card/directory-card.component';
+import { NoteCardComponent } from './notemon-card/note-card/note-card.component';
+import { NotemonCardEmptyComponent } from './notemon-card/notemon-card-empty/notemon-card-empty.component';
+import { NotemonCardVariantComponent } from './notemon-card/notemon-card-variant/notemon-card-variant.component';
+import { NotemonEditorComponent } from './notemon-editor/notemon-editor.component';
+import { SharePageComponent } from './share-page/share-page.component';
+import { SnackbarComponent } from './snackbar/snackbar.component';
+import { ThemeToggleButtonComponent } from './theme-toggle-button/theme-toggle-button.component';
+import { TopNavbarEditorComponent } from './top-navbar/top-navbar-editor/top-navbar-editor.component';
 
-import {TopNavbarMainComponent} from './top-navbar/top-navbar-main/top-navbar-main.component';
-import {ManageDashboardBaseComponent} from './manage-dashboard-base/manage-dashboard-base.component';
-import {LoadingProgressBarComponent} from './loading/loading-progress-bar/loading-progress-bar.component';
-import {LoadingProgressSpinnerComponent} from './loading/loading-progress-spinner/loading-progress-spinner.component';
-import {SharePageComponent} from './share-page/share-page.component';
-import {OptionSidebarComponent} from './note-page/option-sidebar/option-sidebar.component';
-import {OptionSidebarItemComponent} from './note-page/option-sidebar/option-sidebar-item/option-sidebar-item.component';
+import { TopNavbarMainComponent } from './top-navbar/top-navbar-main/top-navbar-main.component';
 
 @NgModule({
   imports: [
@@ -84,6 +87,7 @@ import {OptionSidebarItemComponent} from './note-page/option-sidebar/option-side
     SharePageComponent,
     OptionSidebarComponent,
     OptionSidebarItemComponent,
+    SettingsDrawSidebarComponent,
   ],
   exports: [
     CommonModule,
---
+++ app/front/src/app/component/note-page/settings-draw-sidebar/settings-draw-sidebar.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-settings-draw-sidebar',
+  templateUrl: './settings-draw-sidebar.component.html',
+  styleUrls: ['./settings-draw-sidebar.component.scss']
+})
+export class SettingsDrawSidebarComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
