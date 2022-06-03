d3e268f6f72f538c8986df935e2f34563ac107da
Phat Duong <beebapcay@gmail.com>
2022-06-03 19:15:31 +0700

    Update `dashboard-page` and add new assests

--- app/front/src/app/component/dashboard-page/dashboard-page.component.ts
+++ app/front/src/app/component/dashboard-page/dashboard-page.component.ts
@@ -1,5 +1,19 @@
-import {Component, OnInit} from '@angular/core';
-import {SubscriptionAwareAbstractComponent} from '../subscription-aware.abstract.component';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { finalize, take } from 'rxjs';
+import { AppRouteConstant } from '../../common/app-route.constant';
+import { NotemonTypeEnum } from '../../enum/notemon-type.enum';
+import { SizeEnum } from '../../enum/size.enum';
+import { DirectoryModel } from '../../model/directory.model';
+import { DocumentModel } from '../../model/document.model';
+import { NoteModel } from '../../model/note.model';
+import { UserModel } from '../../model/user.model';
+import { DocumentService } from '../../service/document.service';
+import { LoadingService } from '../../service/loading.service';
+import { SnackbarService } from '../../service/snackbar.service';
+import { UserService } from '../../service/user.service';
+import { ArrayUtil } from '../../utils/array.util';
+import { SubscriptionAwareAbstractComponent } from '../subscription-aware.abstract.component';
 
 @Component({
   selector: 'app-dashboard-page',
@@ -7,11 +21,134 @@ import {SubscriptionAwareAbstractComponent} from '../subscription-aware.abstract
   styleUrls: ['./dashboard-page.component.scss']
 })
 export class DashboardPageComponent extends SubscriptionAwareAbstractComponent implements OnInit {
+  insideParent: string;
 
-  constructor() {
+  user: UserModel = null;
+
+  isDashboardPage: boolean = false;
+
+  starreds: DocumentModel[] = [];
+  directories: DirectoryModel[] = [];
+  notes: NoteModel[] = [];
+
+  loading: boolean = true;
+
+  readonly NotemonCardTypeEnum = NotemonTypeEnum;
+  readonly NotemonTypeEnum = NotemonTypeEnum;
+  readonly SizeEnum = SizeEnum;
+  readonly ArrayUtil = ArrayUtil;
+
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private userService: UserService,
+              private documentService: DocumentService,
+              private snackbarService: SnackbarService,
+              private loadingService: LoadingService) {
     super();
   }
 
   ngOnInit(): void {
+    this.registerSubscription(
+      this.route.params.subscribe(params => {
+        this.insideParent = params['directoryId'];
+        if (!this.insideParent) {
+          this.isDashboardPage = true;
+        }
+        this.fetchDocuments();
+      })
+    );
+
+    this.registerSubscription(
+      this.userService.user.subscribe(user => {
+        this.user = user;
+        if (this.user !== null) this.fetchDocuments();
+      })
+    )
+
+    this.registerSubscription(
+      this.documentService.change.subscribe(() => {
+        if (this.user === null) return;
+        this.fetchDocuments();
+      })
+    );
+
+    this.registerSubscription(
+      this.documentService.source.subscribe(() => {
+        this.sourceDataFilter();
+      })
+    )
+
+    this.registerSubscription(
+      this.loadingService.loadingSpinner.subscribe(loading => {
+        this.loading = loading;
+      })
+    );
   }
+
+  fetchDocuments() {
+    this.loadingService.showLoadingSpinner();
+    this.loadingService.showLoadingBar();
+    if (this.user === null) return;
+    this.registerSubscription(
+      this.userService.getAllDocuments(this.user?.id, this.insideParent, null)
+        .pipe(take(1), finalize(() => {
+          this.loadingService.hideLoadingBar();
+          this.loadingService.hideLoadingSpinner();
+        }))
+        .subscribe({
+          next: (documents) => {
+            this.documentService.source.next(documents ?? []);
+          },
+          error: error => {
+            this.snackbarService.openRequestErrorAnnouncement(error);
+            this.router.navigate([`/${AppRouteConstant.PAGE_NOT_FOUND}`], {replaceUrl: true}).then();
+          }
+        })
+    );
+
+  }
+
+
+  sourceDataFilter() {
+    let documents: DocumentModel[] = this.documentService.source.getValue();
+
+    documents = documents
+      .sort((a, b) => (new Date(a?.createdAt)).getTime() - (new Date(b?.createdAt)).getTime())
+      .reverse();
+
+    this.starreds = documents
+      .filter(document => document?.relationship?.isStarred);
+
+    this.directories = documents
+      .filter(document => document?.isDirectory)
+      .map(document => document as DirectoryModel);
+
+    this.notes = documents
+      .filter(document => !document?.isDirectory)
+      .map(document => document as NoteModel);
+  }
+
+  onNewDocumentClicked(type: NotemonTypeEnum) {
+    if (this.user === null) {
+      this.snackbarService.openErrorAnnouncement('You must be logged in to create a new document.');
+      this.router.navigate(['/', AppRouteConstant.LOGIN]).then();
+      return;
+    }
+
+    let document: DocumentModel = null;
+    if (type === NotemonTypeEnum.DIRECTORY) {
+      document = DirectoryModel.create();
+      this.directories = [document as DirectoryModel, ...this.directories];
+    } else if (type === NotemonTypeEnum.NOTE) {
+      document = NoteModel.create();
+      this.notes = [document as NoteModel, ...this.notes];
+    } else {
+      this.snackbarService.openErrorAnnouncement('Something was wrong. Unknown document type');
+      return;
+    }
+
+    document.author = this.user;
+    document.parent = this.insideParent;
+  }
+
 }
